c8b032a2151d6faea62885fddf3e70de0325a89a
Henrique Rosa <henrique.rosa@meta.com.br>
2020-05-15 17:14:19 -0300

    added multiplatform

---
+++ lib/ui/ios/pages/index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IndexIOS  extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
